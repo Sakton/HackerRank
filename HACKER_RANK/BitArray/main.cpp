86362e375d1ecf0d6b5630ae888712e908310d06
sakton <sakton@mail.ru>
2020-12-03 10:16:46 +0300

    bitArray

--- HACKER_RANK/BitArray/main.cpp
+++ HACKER_RANK/BitArray/main.cpp
@@ -9,55 +9,36 @@
 test 3
 100000000 923092883 976061291 1205350525
 ответ
-100000000 (верно)
+100000000
+
+
+test  100000000 1 3 1
+ответ
+100000000
+
+100000000 1232077670 126810854 1536183938
+ответ 26
+
+
+100000000 569099406 1607140150 823906344
+ответ 31
 */
 
 using namespace std;
 
+// ВОТ!!!
 int main( ) {
   int64_t n, s, p, q;
   cin >> n >> s >> p >> q;
-  std::vector< int64_t > a( n );
-  const uint64_t MASK = 1 << 31;
-  a[ 0 ] = s % MASK;
-  std::set< int64_t > aa;
-  aa.insert( a[ 0 ] );
-  for ( int i = 1; i < n; ++i ) {
-    aa.insert( a[ i ] = ( a[ i - 1 ] * p + q ) % MASK );
+  uint64_t MASK = 2147483648;
+  int64_t m = s % MASK;
+  std::cout << "m = " << m << std::endl;
+  int64_t x = m;
+  int64_t i = 1;
+  for ( ; i < n; ++i ) {
+    int64_t t = x;
+    x = ( x * p + q ) % MASK;
+    if ( x == m || t == x ) break;
   }
-  //  std::sort( a.begin( ), a.end( ) );
-  //  a.erase( std::unique( a.begin( ), a.end( ) ), a.end( ) );
-  cout << aa.size( );
+  cout << i;
 }
-
-//долго!!
-// int main( ) {
-//  int64_t n, s, p, q;
-//  cin >> n >> s >> p >> q;
-//  std::vector< int64_t > a( n );
-//  const uint64_t MASK = 1 << 31;
-//  a[ 0 ] = s % MASK;
-//  std::set< int64_t > aa;
-//  aa.insert( a[ 0 ] );
-//  for ( int i = 1; i < n; ++i ) {
-//    aa.insert( a[ i ] = ( a[ i - 1 ] * p + q ) % MASK );
-//  }
-//  //  std::sort( a.begin( ), a.end( ) );
-//  //  a.erase( std::unique( a.begin( ), a.end( ) ), a.end( ) );
-//  cout << aa.size( );
-//}
-
-//долго!! но тест проходит ... минута
-// int main( ) {
-//  int64_t n, s, p, q;
-//  cin >> n >> s >> p >> q;
-//  std::vector< int64_t > a( n );
-//  const uint64_t MASK = 1 << 31;
-//  a[ 0 ] = s % MASK;
-//  for ( int i = 1; i < n; ++i ) {
-//    a[ i ] = ( a[ i - 1 ] * p + q ) % MASK;
-//  }
-//  std::sort( a.begin( ), a.end( ) );
-//  a.erase( std::unique( a.begin( ), a.end( ) ), a.end( ) );
-//  cout << a.size( );
-//}
