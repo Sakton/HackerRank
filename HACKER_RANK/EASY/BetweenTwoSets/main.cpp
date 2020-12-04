17092a8c2be47daf005dfc4c60161a592619222b
RomanRazumov <sakton@mail.ru>
2020-12-04 18:30:55 +0300

    pr

--- HACKER_RANK/EASY/BetweenTwoSets/main.cpp
+++ HACKER_RANK/EASY/BetweenTwoSets/main.cpp
@@ -8,31 +8,15 @@ using namespace std;
 //  { 3, 9, 6 }, { 36, 72 } - ответ 2
 
 int getTotalX( vector< int > a, vector< int > b ) {
-  vector< int > c;
-  for ( auto el_b : b ) {
-    c.push_back( el_b );
-    for ( auto el_a : a ) {
-      c.push_back( el_a );
-      if ( !( el_b % el_a ) ) {
-        c.push_back( el_b / el_a );
-      }
-    }
+  auto start = *std::max_element( a.cbegin( ), a.cend( ) );
+  auto end = *std::min_element( b.cbegin( ), b.cend( ) );
+  int res = 0;
+  for ( auto i = start; i <= end; ++i ) {
+    bool f1 = std::none_of( a.cbegin( ), a.cend( ), [ & ]( int el ) { return ( i % el ); } );
+    bool f2 = std::none_of( b.cbegin( ), b.cend( ), [ & ]( int el ) { return ( el % i ); } );
+    if ( f1 && f2 ) ++res;
   }
-  std::sort( c.begin( ), c.end( ) );
-  c.erase( std::unique( c.begin( ), c.end( ) ), c.end( ) );
-
-  for ( auto el : c ) cout << el << ' ';
-  cout << endl;
-
-  vector< int > d;
-  for ( auto el_c : c ) {
-    bool f1 = std::none_of( a.cbegin( ), a.cend( ), [ = ]( int x ) { return ( el_c % x ); } );
-    bool f2 = std::none_of( b.cbegin( ), b.cend( ), [ = ]( int x ) { return ( x % el_c ); } );
-    if ( f1 && f2 ) d.push_back( el_c );
-  }
-  for ( auto el_d : d ) cout << el_d << ' ';
-  cout << endl;
-  return d.size( );
+  return res;
 }
 
 void osnovnayaTeoremaArifmetici( int n ) {
@@ -54,10 +38,10 @@ void osnovnayaTeoremaArifmetici( int n ) {
 
 int main( ) { /*std::cout << */
 
-  osnovnayaTeoremaArifmetici( 1164 );
+  // osnovnayaTeoremaArifmetici( 1164 );
 
-  // getTotalX( { 2, 4 }, { 16, 32, 96 } );
+  // std::cout << getTotalX( { 2, 4 }, { 16, 32, 96 } );
   // getTotalX( { 2, 6 }, { 24, 36 } );
-  // getTotalX( { 3, 4 }, { 24, 48 } );
-  // getTotalX( { 3, 9, 6 }, { 36, 72 } );
+  std::cout << getTotalX( { 3, 4 }, { 24, 48 } );
+  // std::cout << getTotalX( { 3, 9, 6 }, { 36, 72 } );
 }
