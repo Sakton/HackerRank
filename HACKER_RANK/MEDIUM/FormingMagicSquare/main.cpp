ce25dd9cb8255afed6cf9707ab3be2275287f98f
RomanRazumov <sakton@mail.ru>
2020-12-08 18:07:11 +0300

    pr

--- HACKER_RANK/MEDIUM/FormingMagicSquare/main.cpp
+++ HACKER_RANK/MEDIUM/FormingMagicSquare/main.cpp
@@ -15,7 +15,13 @@ int main()
   std::cout << formingMagicSquare( input );
 }
 
-int formingMagicSquare( std::vector< std::vector< int > > s ) {}
+int formingMagicSquare( std::vector< std::vector< int > > s ) {
+  std::vector< std::vector< int > > baseVariantesQuadrat = {
+      { 8, 1, 6, 3, 5, 7, 4, 9, 2 }, { 6, 1, 8, 7, 5, 3, 2, 9, 4 }, { 4, 9, 2, 3, 5, 7, 8, 1, 6 }, { 2, 9, 4, 7, 5, 3, 6, 1, 8 },
+      { 8, 3, 4, 1, 5, 9, 6, 7, 2 }, { 4, 3, 8, 9, 5, 1, 2, 7, 6 }, { 6, 7, 2, 1, 5, 9, 8, 3, 4 }, { 2, 7, 6, 9, 5, 1, 4, 3, 8 },
+  };
+  // FIXME тупо пройти вычислить и выбрать минимум
+}
 
 /*
 int formingMagicSquare( std::vector< std::vector< int > > s ) {
