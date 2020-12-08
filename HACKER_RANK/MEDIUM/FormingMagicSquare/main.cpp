9cce71edb269eb0fe99b4d30f131efa3b3895bb1
RomanRazumov <sakton@mail.ru>
2020-12-08 21:58:42 +0300

    pr

--- HACKER_RANK/MEDIUM/FormingMagicSquare/main.cpp
+++ HACKER_RANK/MEDIUM/FormingMagicSquare/main.cpp
@@ -10,8 +10,8 @@ int main()
   // std::vector< std::vector< int > > input { { 5, 3, 4 }, { 1, 5, 8 }, { 6, 4, 2 } };
   // std::vector< std::vector< int > > input { { 3, 3, 3 }, { 3, 3, 3 }, { 3, 3, 3 } };
   // std::vector< std::vector< int > > input { { 4, 8, 2 }, { 4, 5, 7 }, { 6, 1, 6 } };
-  // std::vector< std::vector< int > > input { { 6, 7, 8 }, { 7, 6, 2 }, { 3, 2, 3 } };  // 17
-  std::vector< std::vector< int > > input { { 2, 9, 8 }, { 4, 2, 7 }, { 5, 6, 7 } };  // 21
+  std::vector< std::vector< int > > input { { 6, 7, 8 }, { 7, 6, 2 }, { 3, 2, 3 } };  // 17
+  // std::vector< std::vector< int > > input { { 2, 9, 8 }, { 4, 2, 7 }, { 5, 6, 7 } };  // 21
   std::cout << formingMagicSquare( input );
 }
 
@@ -19,7 +19,24 @@ int formingMagicSquare( std::vector< std::vector< int > > s ) {
   std::vector< std::vector< int > > baseVariantesQuadrat = {
       { 8, 1, 6, 3, 5, 7, 4, 9, 2 }, { 6, 1, 8, 7, 5, 3, 2, 9, 4 }, { 4, 9, 2, 3, 5, 7, 8, 1, 6 }, { 2, 9, 4, 7, 5, 3, 6, 1, 8 },
       { 8, 3, 4, 1, 5, 9, 6, 7, 2 }, { 4, 3, 8, 9, 5, 1, 2, 7, 6 }, { 6, 7, 2, 1, 5, 9, 8, 3, 4 }, { 2, 7, 6, 9, 5, 1, 4, 3, 8 } };
-  // FIXME тупо пройти вычислить и выбрать минимум
+  std::vector< int > squareLine( 9 );
+  using size_type = std::vector< int >::size_type;
+  size_type k = 0;
+  for ( auto it = s.cbegin( ); it < s.cend( ); ++it ) {
+    for ( auto jt = it->cbegin( ); jt < it->cend( ); ++jt ) {
+      squareLine[ k++ ] = *jt;
+    }
+  }
+  int total = std::numeric_limits< int >::max( );
+  int res = 0;
+  for ( size_type i = 0; i < baseVariantesQuadrat.size( ); ++i ) {
+    for ( size_type j = 0; j != baseVariantesQuadrat[ i ].size( ); ++j ) {
+      res += std::abs( baseVariantesQuadrat[ i ][ j ] - squareLine[ j ] );
+    }
+    total = std::min( total, res );
+    res = 0;
+  }
+  return total;
 }
 
 /*
