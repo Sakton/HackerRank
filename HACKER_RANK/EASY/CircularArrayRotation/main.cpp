91217104144cabd4f751b2362bcea08396abebb0
sakton <sakton@mail.ru>
2020-12-11 13:01:24 +0300

    pr

--- HACKER_RANK/EASY/CircularArrayRotation/main.cpp
+++ HACKER_RANK/EASY/CircularArrayRotation/main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 #include <vector>
 
-std::vector< int > circularArrayRotation( std::vector< int > a, int k, std::vector< int > queries ) {}
+std::vector< int > circularArrayRotation( std::vector< int > a, int k, std::vector< int > queries );
 
-int main()
-{
-
+int main( ) {
+  std::vector< int > t = circularArrayRotation( { 1, 2, 3 }, 2, { 0, 1, 2 } );
+  for ( auto el : t ) std::cout << el << " ";
 }
+
+std::vector< int > circularArrayRotation( std::vector< int > a, int k, std::vector< int > queries ) {
+  std::vector< int > res( queries.size( ) );
+  k %= a.size( );
+  for ( int index : queries ) res[ index ] = a[ ( index + a.size( ) - k ) % a.size( ) ];
+  return res;
+}
+
+//влево
+// std::vector< int > circularArrayRotation( std::vector< int > a, int k, std::vector< int > queries ) {
+//  std::vector< int > res( queries.size( ) );
+//  for ( int index : queries ) res[ index ] = a[ ( index - ( k - a.size( ) ) ) % a.size( ) ];
+//  return res;
+//}
