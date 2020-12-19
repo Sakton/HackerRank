e34a7bbc017628805819ed26e09dc40158ee01d1
RomanRazumov <sakton@mail.ru>
2020-12-19 23:07:09 +0300

    pr

--- HACKER_RANK/EULER/ProjectEuler3_LargestPrimeFactor/main.cpp
+++ HACKER_RANK/EULER/ProjectEuler3_LargestPrimeFactor/main.cpp
@@ -1,16 +1,34 @@
 #include <iostream>
 #include <vector>
 
+// int64_t maxFactor( int64_t N ) {
+//  while ( !( N % 2 ) ) N /= 2;
+//  int64_t max = 2;
+//  int64_t d = 3;
+//  while ( N > 1 ) {
+//    max = d;
+//    while ( N > 1 && !( N % d ) ) N /= d;
+//    d += 2;
+//  }
+//  return max;
+//}
+
 int64_t maxFactor( int64_t N ) {
   while ( !( N % 2 ) ) N /= 2;
-  int64_t max = 2;
+  int64_t max = 1;
   int64_t d = 3;
-  while ( N > 1 ) {
-    max = d;
-    while ( N > 1 && !( N % d ) ) N /= d;
+  int64_t maxF = static_cast< int64_t >( std::sqrt( N ) );
+  while ( N > 1 && d <= maxF ) {
+    if ( !( N % d ) ) {
+      N /= d;
+      max = d;
+      while ( N > 1 && !( N % d ) ) N /= d;
+      maxF = static_cast< int64_t >( std::sqrt( N ) );
+    }
     d += 2;
   }
-  return max;
+  if ( N == 1 ) return max;
+  return N;
 }
 
-int main( ) { std::cout << maxFactor( 13195 ) << std::endl; }
+int main( ) { std::cout << maxFactor( 1001 ) << std::endl; }
