6a542c655c3c32c127593b6ab660a19f73bd04e4
sakton <sakton@mail.ru>
2020-12-21 16:20:30 +0300

    pr

--- HACKER_RANK/MEDIUM/MEDIUM.pro
+++ HACKER_RANK/MEDIUM/MEDIUM.pro
@@ -5,4 +5,5 @@ SUBDIRS += \
 	ExtraLongFactorials \
 	FormingMagicSquare \
 	NonDivisibleSubset \
+	OrganizingContainersOfBalls \
 	QueensAttackII
---
+++ HACKER_RANK/MEDIUM/OrganizingContainersOfBalls/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+// std::string organizingContainers( std::vector< std::vector< int64_t > > container ) {
+//  int size = container.size( );
+//  std::string answer = "Possible";
+//  for ( int i = 0; i < size - 1; ++i ) {
+//    //**
+//    for ( int row = i + 1, col = i + 1; col < size; ++row, ++col ) {
+//      if ( container[ row ][ i ] != container[ i ][ col ] ) {
+//        return answer = "Impossible";
+//      }
+//    }
+//  }
+//  return answer;
+//}
+
+std::string organizingContainers( std::vector< std::vector< int64_t > > container ) {
+  using sz = std::vector< int64_t >::size_type;
+  sz size = container.size( );
+  for ( sz curRow = 0; curRow < size - 1; ++curRow ) {
+    sz interressantCol = curRow;
+    sz nextRow = curRow + 1;
+    for ( ; nextRow < size; ++nextRow ) {
+      for ( sz nextCol = interressantCol + 1; nextCol < size && container[ nextRow ][ interressantCol ] > 0; ++nextCol ) {
+        if ( container[ curRow ][ nextCol ] ) {
+          int16_t delta = std::min( container[ curRow ][ nextCol ], container[ nextRow ][ interressantCol ] );
+          container[ curRow ][ interressantCol ] += delta;
+          container[ curRow ][ nextCol ] -= delta;
+          container[ nextRow ][ nextCol ] += delta;
+        }
+      }
+    }
+  }
+  return { };
+}
+
+int main( ) {
+  // std::cout << organizingContainers( { { 1, 1 }, { 1, 1 } } );
+  // std::cout << organizingContainers( { { 1, 3, 1 }, { 2, 1, 2 }, { 3, 3, 3 } } );
+  std::cout << organizingContainers( { { 0, 2, 1 }, { 1, 1, 1 }, { 2, 0, 0 } } );
+}
