b5fe9243c2b61001c7551e4c59d804f242fd00d0
sakton <sakton@mail.ru>
2021-04-02 09:40:09 +0300

    FrequencyQueries(map)

--- HACKER_RANK/MEDIUM/FrequencyQueries/FrequencyQueries.pro
+++ HACKER_RANK/MEDIUM/FrequencyQueries/FrequencyQueries.pro
@@ -5,3 +5,7 @@ CONFIG -= qt
 
 SOURCES += \
         main.cpp
+
+DISTFILES += \
+	ans.txt \
+	input.txt
--- HACKER_RANK/MEDIUM/FrequencyQueries/main.cpp
+++ HACKER_RANK/MEDIUM/FrequencyQueries/main.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include <iostream>
 #include <map>
 #include <vector>
@@ -8,28 +9,31 @@ using namespace std;
 
 vector< int > freqQuery( vector< vector< int > > queries ) {
   vector< int > res;
-  map< int, int > tmp;
+  map< int, int > tmp;   //число - частота
+  map< int, int > temp;  //частота - количество
   for ( const auto& el : queries ) {
     int query = el[ 0 ];
     int param = el[ 1 ];
     switch ( query ) {
-      case 1:
-        tmp[ param ]++;
-        break;
+      case 1: {
+        if ( tmp[ param ] > 0 ) {
+          temp[ tmp[ param ] ]--;
+          tmp[ param ]++;
+          temp[ tmp[ param ] ]++;
+        } else {
+          tmp[ param ]++;
+          temp[ tmp[ param ] ]++;
+        }
+      } break;
       case 2: {
-        auto tt = tmp.find( param );
-        if ( tt != tmp.end( ) ) tmp[ param ]--;
-        if ( tmp[ param ] <= 0 ) tmp.erase( param );
+        if ( tmp[ param ] > 0 ) {
+          temp[ tmp[ param ] ]--;
+          tmp[ param ]--;
+          temp[ tmp[ param ] ]++;
+        }
       } break;
       case 3: {
-        auto t = std::find_if( tmp.cbegin( ), tmp.cend( ),
-                               [ &param ]( std::pair< int, int >& pr ) {
-                                 return param == pr.second;
-                               } );
-        if ( t != tmp.end( ) )
-          res.push_back( 1 );
-        else
-          res.push_back( 0 );
+        ( temp[ param ] > 0 ) ? res.push_back( 1 ) : res.push_back( 0 );
       } break;
     }
   }
@@ -37,8 +41,38 @@ vector< int > freqQuery( vector< vector< int > > queries ) {
 }
 
 int main( ) {
-  vector< vector< int > > v { { 1, 5 },  { 1, 6 }, { 3, 2 }, { 1, 10 },
-                              { 1, 10 }, { 1, 6 }, { 2, 5 }, { 3, 2 } };
+  //  vector< vector< int > > v { { 1, 5 },  { 1, 6 }, { 3, 2 }, { 1, 10 },
+  //                              { 1, 10 }, { 1, 6 }, { 2, 5 }, { 3, 2 } };
+  vector< vector< int > > v { { 1, 5 }, { 1, 5 }, { 1, 5 }, { 2, 5 }, { 1, 4 },
+                              { 1, 4 }, { 1, 4 }, { 2, 4 }, { 2, 4 } };
   auto r = freqQuery( v );
   for ( int e : r ) std::cout << e << " ";
+
+  //  v.clear( );
+  //  std::ifstream ifile(
+  //      "E:/CPP/MyProects/HACKER_RANK/HackerRank/HACKER_RANK/MEDIUM/"
+  //      "FrequencyQueries/input.txt" );
+  //  int count = 0;
+  //  ifile >> count;
+  //  while ( count-- ) {
+  //    int a, b;
+  //    ifile >> a >> b;
+  //    v.push_back( { a, b } );
+  //  }
+  //  auto r = freqQuery( v );
+  //  // for ( auto el : r ) std::cout << el << " ";
+
+  //  std::ifstream ifile2(
+  //      "E:/CPP/MyProects/HACKER_RANK/HackerRank/HACKER_RANK/MEDIUM/"
+  //      "FrequencyQueries/ans.txt",
+  //      std::ifstream::binary );
+
+  //  std::vector< int > an;
+  //  std::copy( std::istream_iterator< int >( ifile2 ),
+  //             std::istream_iterator< int >( ), std::back_insert_iterator( an
+  //             ) );
+
+  //  for ( int i = 0; i < an.size( ); ++i ) {
+  //    if ( r[ i ] != an[ i ] ) std::cout << r[ i ] << " ";
+  //  };
 }
